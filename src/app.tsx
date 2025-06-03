3e52151611d57454bc32fb27552d73dba39e5099
Grzegorz Dubiel <greg2012201@gmail.com>
2025-06-03 22:00:22 +0200

    Update app with the new components.

--- src/app.tsx
+++ src/app.tsx
@@ -1,20 +1,10 @@
 import "./app.css";
-import { Button } from "./components/button";
-import { useEffect, useRef, useState } from "react";
-import {
-    AlertClose,
-    AlertConfirm,
-    AlertDialog,
-    AlertDialogContent,
-    AlertDialogDescription,
-    AlertDialogTrigger,
-    AlertOptions,
-} from "./components/alert-dialog";
-import Link from "./components/link";
+import { useEffect, useRef } from "react";
+import { ITEMS } from "./const/listData";
+import { ListExample } from "./components/list-example";
 
 function App() {
     const buttonRef = useRef<HTMLButtonElement | null>(null);
-    const [isOpen, setIsOpen] = useState(false);
 
     useEffect(() => {
         console.log("Button ref:", buttonRef.current);
@@ -23,30 +13,7 @@ function App() {
         <div className="flex flex-col space-y-4 items-center justify-center h-screen">
             <h1>Reusable components playground</h1>
             <div className="space-x-4">
-                <Button variant="outline" size="lg" onClick={() => setIsOpen(true)}>
-                    Click me
-                </Button>
-                <Button ref={buttonRef} asChild variant="link">
-                    <Link
-                        href="https://www.aboutjs.dev"
-                        className="text-blue-500"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Give me to the awesome blog
-                    </Link>
-                </Button>
-                <AlertDialog onOpenChange={setIsOpen} isOpen={isOpen}>
-                    {/* <AlertDialog onOpenChange={setIsOpen} isOpen={isOpen}> */}
-                    <AlertDialogTrigger>Open Dialog</AlertDialogTrigger>
-                    <AlertDialogContent>
-                        <AlertDialogDescription>Hello! I am a little alert</AlertDialogDescription>
-                        <AlertOptions>
-                            <AlertClose>Close</AlertClose>
-                            <AlertConfirm>Confirm</AlertConfirm>
-                        </AlertOptions>
-                    </AlertDialogContent>
-                </AlertDialog>
+                <ListExample items={ITEMS} />
             </div>
         </div>
     );
