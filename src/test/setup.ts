b5b0e519430bddd4258eeff1edf134233fc78603
Grzegorz Dubiel <greg2012201@gmail.com>
2025-06-07 20:48:47 +0200

    Add matchers.

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,6 +1,9 @@
-import "@testing-library/jest-dom";
-import { afterEach } from "vitest";
+import "@testing-library/jest-dom/vitest";
+import { afterEach, expect } from "vitest";
 import { cleanup } from "@testing-library/react";
+import * as matchers from "@testing-library/jest-dom/matchers";
+
+expect.extend(matchers);
 
 // runs a cleanup after each test case
 afterEach(() => {
