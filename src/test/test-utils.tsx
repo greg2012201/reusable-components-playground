6984a7d1f12baede8cfd346517f2c6620ed0612d
Grzegorz Dubiel <greg2012201@gmail.com>
2025-06-24 22:19:59 +0200

    Add a test util for reading file content.

--- src/test/test-utils.tsx
+++ src/test/test-utils.tsx
@@ -1,9 +1,17 @@
 import { render } from "@testing-library/react";
 import { type ReactElement } from "react";
 
+function readFileContent(file: File): Promise<string> {
+    return new Promise<string>((resolve, reject) => {
+        const reader = new FileReader();
+        reader.onload = (e: ProgressEvent<FileReader>) => resolve(e.target?.result as string);
+        reader.onerror = (e: ProgressEvent<FileReader>) => reject(e);
+        reader.readAsText(file);
+    });
+}
+
 function customRender(ui: ReactElement, options = {}) {
     return render(ui, {
-        // wrap provider if needed
         wrapper: ({ children }) => children,
         ...options,
     });
@@ -11,3 +19,4 @@ function customRender(ui: ReactElement, options = {}) {
 
 export * from "@testing-library/react";
 export { customRender as render };
+export { readFileContent };
